c838733e0695f55e1f69115b5614c2e463c2f22b
Fernando Dias <frpdias@icloud.com>
2025-11-24 09:44:44 -0300

    🎨 Atualizar logo para LOGO3.png sem fundo
    - Substituir logo.png pela LOGO3.png
    - Logo3 tem transparência e sem fundo
    - Melhor visual no header

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["images/hero-factory.jpg","images/logo.png","images/logo3.png","images/logo4.png"]),
 	mimeTypes: {".jpg":"image/jpeg",".png":"image/png"},
 	_: {
-		client: {start:"_app/immutable/entry/start.BrwmHrIS.js",app:"_app/immutable/entry/app.q4D9K007.js",imports:["_app/immutable/entry/start.BrwmHrIS.js","_app/immutable/chunks/BsBjc5Hn.js","_app/immutable/chunks/Cic3LXh-.js","_app/immutable/chunks/DiQ5VlDJ.js","_app/immutable/entry/app.q4D9K007.js","_app/immutable/chunks/Cic3LXh-.js","_app/immutable/chunks/tHfPhL_P.js","_app/immutable/chunks/BdPJ2nMw.js","_app/immutable/chunks/DiQ5VlDJ.js","_app/immutable/chunks/CJF7e3YA.js","_app/immutable/chunks/A2Km-dPo.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.D_lJdGuk.js",app:"_app/immutable/entry/app.D9kOVBjM.js",imports:["_app/immutable/entry/start.D_lJdGuk.js","_app/immutable/chunks/Bd7dRIUr.js","_app/immutable/chunks/Cic3LXh-.js","_app/immutable/chunks/DiQ5VlDJ.js","_app/immutable/entry/app.D9kOVBjM.js","_app/immutable/chunks/Cic3LXh-.js","_app/immutable/chunks/tHfPhL_P.js","_app/immutable/chunks/BdPJ2nMw.js","_app/immutable/chunks/DiQ5VlDJ.js","_app/immutable/chunks/CJF7e3YA.js","_app/immutable/chunks/A2Km-dPo.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
