fac0e8415e6f25ad1d5bcf0d653c457c71461cf8
Fernando Dias <frpdias@icloud.com>
2025-11-24 09:45:46 -0300

    🎨 Atualizar logo para LOGO4.png
    - Substituir logo.png pela LOGO4.png
    - Nova identidade visual no header

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.B7cQA581.js","_app/immutable/chunks/BdPJ2nMw.js","_app/immutable/chunks/Cic3LXh-.js","_app/immutable/chunks/CuQwhsa9.js","_app/immutable/chunks/tHfPhL_P.js","_app/immutable/chunks/BLsWcdAw.js","_app/immutable/chunks/Bd7dRIUr.js","_app/immutable/chunks/DiQ5VlDJ.js"];
+export const imports = ["_app/immutable/nodes/1.V4dldMW4.js","_app/immutable/chunks/BdPJ2nMw.js","_app/immutable/chunks/Cic3LXh-.js","_app/immutable/chunks/CuQwhsa9.js","_app/immutable/chunks/tHfPhL_P.js","_app/immutable/chunks/BLsWcdAw.js","_app/immutable/chunks/CUIITLlZ.js","_app/immutable/chunks/DiQ5VlDJ.js"];
 export const stylesheets = [];
 export const fonts = [];
