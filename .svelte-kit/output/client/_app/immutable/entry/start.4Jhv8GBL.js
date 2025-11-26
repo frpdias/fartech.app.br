4d2ca078dc01961b70a6f9999609b4d72b581c00
Fernando Dias <frpdias@icloud.com>
2025-11-26 19:54:50 -0300

    ✨ Apply Stripe.com spacing standard - Increase container padding for premium layout (40-120px margins)

---
+++ .svelte-kit/output/client/_app/immutable/entry/start.4Jhv8GBL.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/Cqy1O8eK.js";export{o as load_css,r as start};
--- .svelte-kit/output/server/nodes/0.js
+++ .svelte-kit/output/server/nodes/0.js
@@ -3,6 +3,6 @@
 export const index = 0;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.tW7dISR8.js","_app/immutable/chunks/DqYnEuf9.js","_app/immutable/chunks/BUB4oGmN.js","_app/immutable/chunks/C3WsJ1qc.js","_app/immutable/chunks/feqk1icz.js","_app/immutable/chunks/CY8AB-us.js","_app/immutable/chunks/DYjfvAIA.js","_app/immutable/chunks/Ba3cHqJH.js","_app/immutable/chunks/CBFl0TFB.js","_app/immutable/chunks/BiDgMlbb.js","_app/immutable/chunks/itv6a9Na.js","_app/immutable/chunks/BXSozWrX.js","_app/immutable/chunks/TyfZ3v9D.js"];
-export const stylesheets = ["_app/immutable/assets/0.CcSq_y2e.css"];
+export const imports = ["_app/immutable/nodes/0.CiV2lA2x.js","_app/immutable/chunks/DqYnEuf9.js","_app/immutable/chunks/BUB4oGmN.js","_app/immutable/chunks/C3WsJ1qc.js","_app/immutable/chunks/feqk1icz.js","_app/immutable/chunks/CY8AB-us.js","_app/immutable/chunks/DYjfvAIA.js","_app/immutable/chunks/Ba3cHqJH.js","_app/immutable/chunks/CBFl0TFB.js","_app/immutable/chunks/BiDgMlbb.js","_app/immutable/chunks/itv6a9Na.js","_app/immutable/chunks/BXSozWrX.js","_app/immutable/chunks/TyfZ3v9D.js"];
+export const stylesheets = ["_app/immutable/assets/0.B8_N5xsE.css"];
 export const fonts = [];
